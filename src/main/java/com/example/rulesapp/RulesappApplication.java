f59f8407a08e7b1850fe971b6e4577e752559916
dmihov79 <76250498+dmihov79@users.noreply.github.com>
2024-06-05 22:26:15 +0300

    Add files via upload

---
+++ src/main/java/com/example/rulesapp/RulesappApplication.java
@@ -0,0 +1,13 @@
+package com.example.rulesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulesappApplication.class, args);
+	}
+
+}
